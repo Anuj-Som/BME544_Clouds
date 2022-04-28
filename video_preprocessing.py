7c6acafc8d096481498652ecdc9eebf4783e1371
Anuj Som <anuj.som@duke.edu>
2022-04-28 14:10:12 -0400

    made preprocessing reshape images to 1000x1000

--- video_preprocessing.py
+++ video_preprocessing.py
@@ -19,18 +19,25 @@ def get_videos(dir):
             path_list.append(f)
     return path_list
 
+def resize_image(img):
+    # Resize image to 1000x1000
+    width = 1000
+    height = 1000
+    dim = (width, height)
+    resized = cv2.resize(img, dim, interpolation = cv2.INTER_AREA)
+    return resized
+
 def main():
     videos = get_videos("./raw_timelapse")
     print(type(videos))
     print(videos)
     print("Number of videos: {}".format(len(videos)))
-    
     for vidnum, videoname in enumerate(videos):
         vidcap = cv2.VideoCapture(videoname)
         success,image = vidcap.read()
         count = 0
         while success:
-            cv2.imwrite("./gan_data/v{}frame{}.jpg".format(vidnum, count), image)     # save frame as JPEG file      
+            cv2.imwrite("./gan_data/v{}frame{}.jpg".format(vidnum, count), resize_image(image))     # save frame as JPEG file      
             success,image = vidcap.read()
             count += 1
         print("Video {}: Complete".format(vidnum))
